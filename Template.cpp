0ee4eccf4e063cee73b8e74956b68afdb963f172
Ilia Rahbar <63718789+iliarahbar@users.noreply.github.com>
2025-02-25 11:24:25 +0330

    add Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,21 @@
+// بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+// Written by Ilia Rahbar
+
+// #pragma GCC optimize ("O3,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target ("abm,bmi,bmi2,tbm,avx2")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define sz(x) int(x.size())
+template <int x> using ai = array <int, x>;
+const char el = '\n', sp = ' ';
+const int M = 1e9 + 7, N = 1e6 + 100;
+
+int32_t main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+}
+
